47b7686239f6df848d0e5be094c67f5eed4d3c6f
JisuPark <53842434+haeju20@users.noreply.github.com>
2022-12-01 18:53:45 +0900

    1201
    - call 함수 version a ->version b
    - nickname

--- server.js
+++ server.js
@@ -1,46 +1,77 @@
 const express = require("express");
-const { ExpressPeerServer } = require("peer");
 const app = express();
+const session = require('express-session');
+const fileStore = require('session-file-store')(session);
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const { v4: uuidV4 } = require("uuid");
-
+const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
     debug: true
-});  
+  });
+const { v4: uuidV4 } = require("uuid");
 
 let studyRoomId;
 let Nickname;
 
+app.use('/peerjs', peerServer);
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(express.urlencoded({extended:false}));      // body-parser
-app.use('/peerjs', peerServer);
+
+/*
+//express-session 세션 - 두 사용자가 한 방에 안 들어오는 문제
+app.use(
+    session({
+        secure: true, 
+        secret: 'keyboard cat',
+        //secret: process.env.COOKIE_SECRET, //암호화
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            httpOnly: true,
+            secure: true,
+        },
+        store: new fileStore() //세션 스토어 적용
+    })
+);
+*/
+
+/*
+app.get("/", (req, res) => {
+    console.log(req.session);
+
+    if(!req.session.num){
+        req.session.num = 1;
+    } else {
+        req.session.num = req.session.num + 1;
+    }
+    res.render("studyHome");
+});
+*/
 
 app.get("/", (req, res) => {
     res.render("studyHome");
 });
+
 app.post("/videoChat", (req, res) => {
+    console.log("roomId: " + req.body.StudyName); 
+    //console.log("userId: " + req.body.userId);
+    console.log("nickname: " + req.body.Nickname);
     studyRoomId = req.body.StudyName;
     Nickname = req.body.Nickname;
-    res.render("videoChat", { roomID: studyRoomId, nickname: Nickname });
+    res.render("videoChat", {roomId: studyRoomId, nickname: Nickname});   
 });
-
 
 io.on("connection", socket => {
 
-
-    socket.on("join-room", (roomId, userId) => {
+    socket.on("join-room", (roomId, userId) => {  
+        //console.log("userId: "+ userId);
         socket.join(roomId);
-        // version A
-        socket.to(roomId).emit("user-connected", userId);
 
-        /*
-        // version B
-        socket.on('connection-request',(roomId, userId) => {
-            socket.to(roomId).emit("new-user-connected", userId);
+        socket.on('connection-request',(roomId, userId, nickname) => {
+            socket.to(roomId).emit("new-user-connected", userId, nickname);
         });
-        */
 
         socket.on("disconnect", () => {
             socket.to(roomId).emit("user-disconnected", userId);
